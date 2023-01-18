cd8c91c1107a89db21ca8a585b64579bd4c51bde
Shilna P <pshilsrihanat1@gmail.com>
2023-01-18 11:06:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage";
+
+import Login from "./components/Users/Login/Login";
+import Register from "./components/Users/Register/Register";
+import Navbar from "./components/Navigation/Navbar";
+import AddNewCategory from "./components/Categories/AddNewCategory";
+import CategoryList from "./components/Categories/CategoryList";
+import UpdateCategory from "./components/Categories/UpdateCategory";
+import UserProtectedRoute from "./components/Navigation/ProtectedRoutes/UserProtectedRoute";
+import AdminProtectedRoute from "./components/Navigation/ProtectedRoutes/AdminProtectedRoute";
+import CreatePost from "./components/Posts/CreatePost";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+        <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+
+          <Route element={<AdminProtectedRoute />}>
+            {/* <Route path="/admin-dashboard" element={<AdminDashboard />} /> */}
+            {/* <Route path="/send-mail" element={<SendEmail />} /> */}
+
+            {/* <Route path="/users" element={<UsersList />} /> */}
+            {/* <Route path="/reported-list" element={<ReportedPost />} /> */}
+
+            <Route path="/create-post" element={<CreatePost />} />
+
+            <Route path="/add-category" element={<AddNewCategory />} />
+
+            <Route path="/update-category/:id" element={<UpdateCategory />} />
+            <Route exact path="/category-list" element={<CategoryList/>} />
+          </Route>
+          
+         
+          {/* <Route path="*" element={<PageNotFound />} /> */}
+        </Routes>
+      </Router>
+    </>
   );
 }
-
 export default App;
---
+++ src/components/Navigation/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import AdminNavbar from "./admin/AdminNavbar";
+import PrivateNavbar from "./private/PrivateNavbar";
+import PublicNavbar from "./public/PublicNavbar";
+
+const Navbar = () => {
+  //ge user from store
+  const state = useSelector(state => state.users);
+  const { userAuth } = state;
+  const isAdmin = userAuth?.isAdmin;
+  console.log(isAdmin);
+  return (
+    <>
+      {isAdmin ? (
+        <AdminNavbar />
+      ) : userAuth ? (
+        <PrivateNavbar />
+      ) : (
+        <PublicNavbar />
+      )}
+    </>
+  );
+};
+
+export default Navbar;
