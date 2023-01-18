b58f57dc457322a91eade75b7a6e1856e1393663
Shilna P <pshilsrihanat1@gmail.com>
2023-01-18 20:18:11 +0530

    fetch catagories completed

--- src/redux/slices/posts/postSlices.js
+++ src/redux/slices/posts/postSlices.js
@@ -1,11 +1,14 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, createAction } from "@reduxjs/toolkit";
 import axios from "axios";
 import baseUrl from "../../../utils/baseUrl";
 //Create Post action
+//action to redirect
+const resetPost = createAction("category/reset");
 
 export const createpostAction = createAsyncThunk(
   "post/created",
   async (post, { rejectWithValue, getState, dispatch }) => {
+    console.log(post);
     //get user token
     const user = getState()?.users;
     const { userAuth } = user;
@@ -16,13 +19,33 @@ export const createpostAction = createAsyncThunk(
     };
     try {
       //http call
-      const formData=new FormData()
-      formData.append('title',post?.title)
-      formData.append('description',post?.description)
-      formData.append('category',post?.category?.label)
-      formData.append('image',post?.image)
+      const formData = new FormData();
+      formData.append("title", post?.title);
+      formData.append("description", post?.description);
+      formData.append("category", post?.category?.label);
+      formData.append("image", post?.image);
 
-      const { data } = await axios.post(`${baseUrl}/api/posts`, formData, config);
+      const { data } = await axios.post(
+        `${baseUrl}/api/posts`,
+        formData,
+        config
+      );
+      //dispatch action
+      dispatch(resetPost());
+      return data;
+    } catch (error) {
+      if (!error?.response) throw error;
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
+//fetch all posts
+export const fetchPostsAction = createAsyncThunk(
+  "post/list",
+  async (post, { rejectWithValue, getState, dispatch }) => {
+    try {
+      const { data } = await axios.get(`${baseUrl}/api/posts`);
       return data;
     } catch (error) {
       if (!error?.response) throw error;
@@ -37,16 +60,36 @@ const postSlice = createSlice({
   name: "post",
   initialState: {},
   extraReducers: builder => {
+    //create post
     builder.addCase(createpostAction.pending, (state, action) => {
       state.loading = true;
+    });
+    builder.addCase(resetPost, (state, action) => {
+      state.isCreated = true;
     });
     builder.addCase(createpostAction.fulfilled, (state, action) => {
       state.postCreated = action?.payload;
       state.loading = false;
+      state.isCreated = false;
       state.appErr = undefined;
       state.serverErr = undefined;
     });
     builder.addCase(createpostAction.rejected, (state, action) => {
+      state.loading = false;
+      state.appErr = action?.payload?.message;
+      state.serverErr = action?.error?.message;
+    });
+    //fetch posts
+    builder.addCase(fetchPostsAction.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchPostsAction.fulfilled, (state, action) => {
+      state.postLists = action?.payload;
+      state.loading = false;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+    });
+    builder.addCase(fetchPostsAction.rejected, (state, action) => {
       state.loading = false;
       state.appErr = action?.payload?.message;
       state.serverErr = action?.error?.message;
