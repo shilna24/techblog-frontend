a928e252e062fe7ba2fcc9719d5baa944e855a80
Shilna P <pshilsrihanat1@gmail.com>
2023-01-18 19:10:58 +0530

    create post updated

--- src/components/Posts/CreatePost.js
+++ src/components/Posts/CreatePost.js
@@ -1,18 +1,19 @@
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import { useDispatch, useSelector } from "react-redux";
+import Dropzone from "react-dropzone";
+import {useNavigate  } from "react-router-dom";
 import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
 import { createpostAction } from "../../redux/slices/posts/postSlices";
 import CategoryDropDown from "../Categories/CategoryDropDown";
-import Dropzone from 'react-dropzone'
+
 //Form schema
 const formSchema = Yup.object({
   title: Yup.string().required("Title is required"),
   description: Yup.string().required("Description is required"),
   category: Yup.object().required("Category is required"),
-  image: Yup.object().required("Image is required"),
+  image: Yup.string().required("Image is required"),
 });
-
 //css for dropzone
 const Container = styled.div`
   flex: 1;
@@ -30,14 +31,19 @@ border-color:'red'
 `;
 
 export default function CreatePost() {
+  const navigate=useNavigate();
   const dispatch = useDispatch();
+
+  //select store data
+  const post = useSelector(state => state?.post);
+  const { isCreated, loading, appErr, serverErr } = post;
   //formik
   const formik = useFormik({
     initialValues: {
       title: "",
       description: "",
       category: "",
-      image:"",
+      image: "",
     },
     onSubmit: values => {
       //dispath the action
@@ -53,6 +59,8 @@ export default function CreatePost() {
     validationSchema: formSchema,
   });
 
+  //redirect
+  if (isCreated) return navigate("/posts")
   return (
     <>
       <div className="min-h-screen bg-gray-900 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -67,6 +75,12 @@ export default function CreatePost() {
               profanity
             </p>
           </p>
+
+          {appErr || serverErr ? (
+            <p className="mt-2 text-center text-lg text-red-600">
+              {serverErr} {appErr}
+            </p>
+          ) : null}
         </div>
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
@@ -97,6 +111,12 @@ export default function CreatePost() {
                 </div>
               </div>
               {/* Category input goes here */}
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Select Category
+              </label>
               <CategoryDropDown
                 value={formik.values.category?.label}
                 onChange={formik.setFieldValue}
@@ -121,13 +141,18 @@ export default function CreatePost() {
                   className="rounded-lg appearance-none block w-full py-3 px-3 text-base text-center leading-tight text-gray-600 bg-transparent focus:bg-transparent  border border-gray-200 focus:border-gray-500  focus:outline-none"
                   type="text"
                 ></textarea>
-
-           {/* image component */}
-           <Container className="container bg-gray-700">
+                {/* Image component */}
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium mt-3 mb-2 text-gray-700"
+                >
+                  Select image to upload
+                </label>
+                <Container className="container bg-gray-700">
                   <Dropzone
                     onBlur={formik.handleBlur("image")}
-                    accept="image/jpeg, image/png image/webp"
-                    onDrop={(acceptedFiles) => {
+                    accept="image/jpeg, image/png"
+                    onDrop={acceptedFiles => {
                       formik.setFieldValue("image", acceptedFiles[0]);
                     }}
                   >
@@ -136,7 +161,7 @@ export default function CreatePost() {
                         <div
                           {...getRootProps({
                             className: "dropzone",
-                            onDrop: (event) => event.stopPropagation(),
+                            onDrop: event => event.stopPropagation(),
                           })}
                         >
                           <input {...getInputProps()} />
@@ -148,7 +173,6 @@ export default function CreatePost() {
                     )}
                   </Dropzone>
                 </Container>
-                
                 {/* Err msg */}
                 <div className="text-red-500">
                   {formik?.touched?.description && formik.errors?.description}
@@ -156,12 +180,21 @@ export default function CreatePost() {
               </div>
               <div>
                 {/* Submit btn */}
-                <button
-                  type="submit"
-                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  Create
-                </button>
+                {loading ? (
+                  <button
+                    disabled
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-600  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Loading please wait...
+                  </button>
+                ) : (
+                  <button
+                    type="submit"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Create
+                  </button>
+                )}
               </div>
             </form>
           </div>
